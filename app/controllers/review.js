69c092390398fa111bf4015a5eb712aa4397e298
Sarah Yeo <sarahyeo2@gmail.com>
2016-03-26 22:49:45 -0700

    One decimal place for rating in media

--- app/controllers/review.js
+++ app/controllers/review.js
@@ -23,7 +23,7 @@ module.exports = function (app, passport) {
             return t.batch([
               t.one(`INSERT INTO Review_Writes_About (comment, rating, userID, mediaID) values ($1, $2, $3, $4) RETURNING *`,
                 [req.body.comment, req.body.rating, req.user.userid, req.body.mediaID]),
-              t.none(`UPDATE Media SET rating=(SELECT AVG(rating) FROM Review_Writes_About WHERE mediaID=$1 GROUP BY mediaID) WHERE mediaID=$1`, [req.body.mediaID])
+              t.none(`UPDATE Media SET rating=(SELECT ROUND(AVG(rating)::numeric, 1) FROM Review_Writes_About WHERE mediaID=$1 GROUP BY mediaID) WHERE mediaID=$1`, [req.body.mediaID])
             ])
           }).then(data => {
             res.redirect(/m/ + req.body.mediaID);
