a60fadbd43cb7e3cd3fc7ec976914320f8c45fd7
mparkms <mikeparkms@gmail.com>
2016-03-28 16:29:37 -0700

    Fix text in textarea not showing up

--- app/controllers/updaterequest.js
+++ app/controllers/updaterequest.js
@@ -1,19 +1,17 @@
 var db = require('../models');
 
 module.exports = function (app, passport) {
-
 	app.route('/ur/')
 		.post((req, res, next) => {
 			db.tx(t => {
 				return t.batch([
 					t.one(`INSERT INTO UpdateRequest_Submits_On_Reviews (submitterID, mediaID, details) values ($1, $2, $3) RETURNING *`,
 						[req.user.userid, req.body.mediaID, req.body.details])
-				])
+				]);
 			}).then(data => {
 				res.redirect(/m/ + req.body.mediaID);
 			}).catch(error => {
 				console.log(error);
 			});
 		});
-
 };
