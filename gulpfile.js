6cbb18ef865a3025c4e3bb1e3952a37d0f44f545
James Liu <jamesjhliu@gmail.com>
2016-03-31 14:03:05 -0700

    Revert "gulpfile.js"
    This reverts commit fdb6dd8907f4e38169f137509386a2d7f1ed9600.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var livereload = require('gulp-livereload');
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload');
 
-gulp.task('develop', () => {
+
+gulp.task('develop', function () {
   livereload.listen();
   nodemon({
     script: 'app.js',
     ext: 'js coffee nunjucks',
     stdout: false
-  }).on('readable', () => {
-    this.stdout.on('data', chunk => {
-      if (/^Express server listening on port/.test(chunk)) {
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
         livereload.changed(__dirname);
       }
     });
