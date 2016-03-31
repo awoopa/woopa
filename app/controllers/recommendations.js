f271c24d749cc7b345c808482d5966c81476671e
Sarah Yeo <sarahyeo2@gmail.com>
2016-03-30 22:00:15 -0700

    changed so uses view in comminuty req query

--- app/controllers/recommendations.js
+++ app/controllers/recommendations.js
@@ -20,33 +20,35 @@ module.exports = function(app) {
                   M.imageID = I.ImageID`,
             [req.user.userid]),
           t.any(`
+            WITH calc AS (
+              SELECT M.mediaid AS mediaid, avg(RWA.rating) as avg, M.title as title, M.type as type  
+                FROM Review_Writes_About RWA, Media M 
+                WHERE RWA.mediaid = M.mediaid GROUP BY M.mediaid)
+
             SELECT M.*, I.*
             FROM Media M, Image I, 
-              (SELECT D.mediaID FROM
-                (SELECT M.type as type,  max(C.avg) AS mx 
-                 FROM  Media M, (SELECT M.mediaid AS mediaid, avg(RWA.rating) as avg, M.title as title, M.type as type  
-                                 FROM Review_Writes_About RWA, Media M 
-                                 WHERE RWA.mediaid = M.mediaid GROUP BY M.mediaid) as C 
-                 WHERE C.mediaid = M.mediaid
-                 GROUP BY M.type) 
-              AS A JOIN (SELECT M.mediaid AS mediaid, avg(RWA.rating) as avg, M.title as title, M.type as type  
-                                             FROM Review_Writes_About RWA, Media M 
-                                             WHERE RWA.mediaid = M.mediaid GROUP BY M.mediaid) as D
-              ON A.type = D.type AND A.mx = D.avg) as TRM
+              (SELECT R.mediaID FROM
+                (SELECT M.type as type,  max(R.avg) AS mx 
+                 FROM  Media M, calc R
+                 WHERE R.mediaid = M.mediaid
+                 GROUP BY M.type) AS A 
+                JOIN calc R ON A.type = R.type AND A.mx = R.avg)  AS TRM
             WHERE M.imageID = I.ImageID AND
-                  TRM.mediaID = M.mediaID
-            EXCEPT
-            SELECT M.*, I.*
-            FROM Recommends_To RT, Media M, WoopaUser U, Image I
-            WHERE RT.recommendeeID = 1 AND
-                  RT.mediaID = M.mediaID AND
-                  U.userID = RT.recommenderID AND
-                  M.imageID = I.ImageID`,
+                  TRM.mediaID = M.mediaID`,
+            // EXCEPT
+            // SELECT M.*, I.*
+            // FROM Recommends_To RT, Media M, WoopaUser U, Image I
+            // WHERE RT.recommendeeID = 1 AND
+            //       RT.mediaID = M.mediaID AND
+            //       U.userID = RT.recommenderID AND
+            //       M.imageID = I.ImageID`,
             [req.user.userid])
         ]);
       })
       .then(data => {
         // populate external recommendations
+        console.log(data[0]);
+
         var results = data[0];
         var recs = [];
         for (var i = 0; i < results.length; i++) {
@@ -59,19 +61,14 @@ module.exports = function(app) {
               });
 
               // check if user is recommending media to themself (e.g. watchlist)
-              if (results[i].email == req.user.email) {
-                recs[j].selfRecommendation = 1;
+              if (results[i].recommenderid == req.user.userid) {
+                recs[j].selfRecommendation = true;
               }
               seen = true;
             }
           }
 
           if (!seen) {
-            // check if user is recommending media to themself (e.g. watchlist)
-            var selfRecommendation = 0;
-            if (results[i].email == req.user.email) {
-              selfRecommendation = 1;
-            }
 
             recs.push({
               mediaid: results[i].mediaid,
@@ -88,36 +85,59 @@ module.exports = function(app) {
               numseasons: results[i].numseasons,
               numviews: results[i].numviews,
               channel: results[i].channel,
-              selfRecommendation: selfRecommendation,
+              selfRecommendation: false,
+              communityRecommendation: false,
               recommenders: [{
                 email: results[i].email,
                 username: results[i].username
               }]
             });
+                  // check if user is recommending media to themself (e.g. watchlist)
+              if (results[i].recommenderid == req.user.userid) {
+                recs[j].selfRecommendation = true;
+              }
           }
         }
 
+
         // populate system suggested media
         var suggested = data[1];
+
         for (var i = 0; i < suggested.length; i++) {
-          recs.push({
-            mediaid: suggested[i].mediaid,
-            title: suggested[i].title,
-            synopsis: suggested[i].synopsis,
-            genre: suggested[i].genre,
-            publishdate: suggested[i].publishdate,
-            rating: suggested[i].rating,
-            img: `data:image/png;base64,${
-              new Buffer(suggested[i].img, 'hex').toString('base64')
-            }`,
-            type: suggested[i].type,
-            runtime: suggested[i].runtime,
-            numseasons: suggested[i].numseasons,
-            numviews: suggested[i].numviews,
-            channel: suggested[i].channel,
-            selfRecommendation: 2,
-            recommenders: []            
-          })
+          var seen = false;
+          for (var j = 0; j < recs.length; j++) {
+            if (results[i].mediaid === recs[j].mediaid) {
+              recs[j].communityRecommendation = true;
+              seen = true;
+            }
+          }
+
+          if (!seen) {
+
+            recs.push({
+              mediaid: results[i].mediaid,
+              title: results[i].title,
+              synopsis: results[i].synopsis,
+              genre: results[i].genre,
+              publishdate: results[i].publishdate,
+              rating: results[i].rating,
+              img: `data:image/png;base64,${
+                new Buffer(results[i].img, 'hex').toString('base64')
+              }`,
+              type: results[i].type,
+              runtime: results[i].runtime,
+              numseasons: results[i].numseasons,
+              numviews: results[i].numviews,
+              channel: results[i].channel,
+              selfRecommendation: false,
+              communityRecommendation: true,
+              recommenders: []
+            });
+            // check if user is recommending media to themself (e.g. watchlist)
+            if (results[i].recommenderid == req.user.userid) {
+              recs[j].communityRecommendation = true;
+            }
+          }
         }
 
         var values = {
