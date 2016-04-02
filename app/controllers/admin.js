97ff9d1b5702cbace149a189196ab65be90ddb79
James Liu <jamesjhliu@gmail.com>
2016-04-01 22:33:53 -0700

    display URs in admin panel

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -3,19 +3,32 @@ var db = require('../models');
 module.exports = function(app) {
   app.route('/admin')
     .get((req, res, next) => {
-      if (req.user.isadmin) {
+      if (req.user && req.user.isadmin) {
         next();
       } else {
-        res.redirect('/');
+        res.status(401);
+        res.render('error', {
+          status: 401,
+          message: "unauthorized",
+          error: {},
+          title: 'error'
+        });
       }
     }, (req, res) => {
       db.tx(t => {
         return t.batch([
-
+          t.any(`
+          SELECT UR.updateID, UR.details, U.username, U.email, U.userID,
+                 M.title, M.mediaID
+          FROM UpdateRequest_Submits_On_Reviews UR,
+               WoopaUser U, Media M
+          WHERE UR.mediaID = M.mediaID AND
+                UR.submitterID = U.userID`)
         ]);
       }).then(data => {
         res.render('admin', {
-          data: data
+          urs: data[0],
+          title: 'Admin'
         });
       });
     });
